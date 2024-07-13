4521cdb3b5d459ab24a02382c108577c63b8615b
huntz <huntzrahmadi@gmail.com>
2024-07-13 08:30:48 +0700

    save progress

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+#! /usr/bin/env bun
+
 import * as cliProgress from "cli-progress";
+import directoryTree from "directory-tree";
 import { program } from "commander";
 import { version } from "./package.json";
 import { Glob } from "bun";
+import { mkdir } from "node:fs/promises";
 
 const glob = new Glob("*");
+
+const directoryChecking = () => {};
 
 program
   .name("leutikin")
@@ -14,32 +20,12 @@ program
   .command("minify")
   .description("Minify resources recursively")
   .argument("<string>", "path to working directory")
-  .option("--path", "display just the first substring")
-  .action((str, options) => {
-    console.log({ str, options });
-
-    for (const file of glob.scanSync(".")) {
-      console.log(file);
-    }
+  .action((str) => {
+    const fileTree = directoryTree(str, {
+      attributes: ["size", "type", "extension"],
+    });
+    const sizedBefore = fileTree.size;
+    mkdir(`.output/${fileTree.name}`, { recursive: true });
   });
 
 program.parse();
-
-// create a new progress bar instance and use shades_classic theme
-// const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-
-// // start the progress bar with a total value of 200 and start value of 0
-// bar1.start(200, 0);
-
-// // update the current value in your application..
-// bar1.update(50);
-// setTimeout(() => {
-// }, 1000)
-// bar1.update(100);
-//  setTimeout(() => {}, 1000)
-// bar1.update(150);
-//  setTimeout(() => {}, 1000)
-// bar1.update(200);
-//  setTimeout(() => {}, 1000)
-// // stop the progress bar
-// bar1.stop();
